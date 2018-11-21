d873b566c377b7764e96a946e27de339a86cded3
notmiro <45168643+notmiro@users.noreply.github.com>
2018-11-21 13:19:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,28 @@
 
-client.on('ready', () => {
-     client.user.setActivity("Text",{type: 'WATCHING'})
-
+const adminprefix = "Mi";
+const devs = ['337398843458977792','337398843458977792'];
+client.on('message', message => {
+  var argresult = message.content.split(`).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'setgame')) {
+  client.user.setGame(argresult);
+} else 
+  if (message.content.startsWith(adminprefix + 'setname')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(${argresult} : تم تغيير أسم البوت إلى`)
+} else
+  if (message.content.startsWith(adminprefix + 'setavatar')) {
+client.user.setAvatar(argresult);
+      } else
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+  } else
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+  } else
+if (message.content.startsWith(adminprefix + 'sett')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/xkilleryt%22);
+}
 });
+
