15eaba57533c1d4254260133ace90ea5471a1397
notmiro <45168643+notmiro@users.noreply.github.com>
2018-11-21 12:35:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,5 @@
-client.on('message', msg => {
-var prefix = "Mistream";//البرفكس
-  var args = msg.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-if (msg.author.bot) return;
-if(msg.content.startsWith(prefix + "setstream")) {
-      if(msg.author.id != "337398843458977792") return msg.reply("هذا الامر لصحاب الحساب");
-      client.user.setGame(`${argresult}`,"http://twitch.tv/S-F")
-      var Die = new Discord.RichEmbed()
-      .setTitle("✅تم تغير الستريمنق الخاص بك")
-     .addField("New Stream",`${argresult}`, true)
-     msg.channel.sendEmbed(Die);
-}
+
+client.on('ready', () => {
+     client.user.setActivity("Text",{type: 'WATCHING'})
+
 });
